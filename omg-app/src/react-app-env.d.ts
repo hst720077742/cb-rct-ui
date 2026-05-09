361aff5002cdd83e998f5c997a9d0c34265f2487
hst720077742 <hst720077742@gmail.com>
2026-05-09 15:08:34 +0000

    tsconfig.js upgrade with google AI

--- omg-app/src/react-app-env.d.ts
+++ omg-app/src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
